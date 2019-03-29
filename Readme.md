007683ae22dae93396a6e7b323ec042bb5ac835b
bughou <applejava@qq.com>
2019-03-29 21:50:32 +0800

    add more funcs

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# strmap
+map with string keys and interface{} values.
+
+[![Build Status](https://travis-ci.org/lovego/strmap.svg?branch=master)](https://travis-ci.org/lovego/strmap)
+[![Coverage Status](https://img.shields.io/coveralls/github/lovego/strmap/master.svg)](https://coveralls.io/github/lovego/strmap?branch=master)
+[![Go Report Card](https://goreportcard.com/badge/github.com/lovego/strmap)](https://goreportcard.com/report/github.com/lovego/strmap)
+[![GoDoc](https://godoc.org/github.com/lovego/strmap?status.svg)](https://godoc.org/github.com/lovego/strmap)
+
+## Install
+`$ go get github.com/lovego/strmap`
+
+
+## Documentation
+[https://godoc.org/github.com/lovego/strmap](https://godoc.org/github.com/lovego/strmap)
--- strmap.go
+++ strmap.go
@@ -37,6 +37,17 @@ func (s StrMap) GetSlice(key string) (results []StrMap) {
 	return
 }
 
+func (s StrMap) GetBool(key string) bool {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToBoolE(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
 func (s StrMap) GetString(key string) string {
 	if s == nil || s[key] == nil {
 		log.Panicf("no key: %s", key)
@@ -53,6 +64,116 @@ func (s StrMap) GetStringSlice(key string) []string {
 		log.Panicf("no key: %s", key)
 	}
 	value, err := cast.ToStringSliceE(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetInt(key string) int {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToIntE(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetInt64(key string) int64 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToInt64E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetInt32(key string) int32 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToInt32E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetInt16(key string) int16 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToInt16E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetInt8(key string) int8 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToInt8E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetUint(key string) uint {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToUintE(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetUint64(key string) uint64 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToUint64E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetUint32(key string) uint32 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToUint32E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetUint16(key string) uint16 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToUint16E(s[key])
+	if err != nil {
+		log.Panic(err)
+	}
+	return value
+}
+
+func (s StrMap) GetUint8(key string) uint8 {
+	if s == nil || s[key] == nil {
+		log.Panicf("no key: %s", key)
+	}
+	value, err := cast.ToUint8E(s[key])
 	if err != nil {
 		log.Panic(err)
 	}
